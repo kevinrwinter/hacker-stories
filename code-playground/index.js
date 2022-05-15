96db228fd08d59fd1c33bfdb8df8f93a25711a7e
Kevin Winter <kevin.winter@edu.bc.fi>
2022-05-15 21:14:56 +0300

    Nested destructuring

--- code-playground/index.js
+++ code-playground/index.js
@@ -1,11 +1,34 @@
-const user = {
+// const user = {
+//   firstName: "Kevin",
+//   lastName: "Winter",
+// };
+
+// // Without object destructuring
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+
+// // With object destructuring
+// const { firstName, lastName } = user;
+
+const petOwner = {
   firstName: "Kevin",
-  lastName: "Winter",
+  pet: {
+    name: "Leevi",
+  },
 };
 
 // Without object destructuring
-const firstName = user.firstName;
-const lastName = user.lastName;
+// const firstName = petOwner.firstName;
+// const name = petOwner.pet.name;
 
-// With object destructuring
-const { firstName, lastName } = user;
+// console.log(firstName + " has a pet called " + name);
+// "Kevin has a pet called Leevi"
+
+// With nested object destructuring
+const {
+  firstName,
+  pet: { name },
+} = petOwner;
+
+console.log(firstName + " has a pet called " + name);
+// "Kevin has a pet called Leevi"
