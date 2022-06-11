b7029c26a9e454537ffaac498eb55b8b20aa10aa
Kevin Winter <kevin.winter@edu.bc.fi>
2022-06-11 15:39:57 +0300

    React component composition

--- src/App.js
+++ src/App.js
@@ -42,7 +42,9 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch} />
+      <InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch}>
+        <strong>Search:</strong>
+      </InputWithLabel>
       <hr />
 
       <List list={searchedStories} />
@@ -50,9 +52,9 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => (
+const InputWithLabel = ({ id, value, type = "text", onInputChange, children }) => (
   <>
-    <label htmlFor={id}>{label}</label>
+    <label htmlFor={id}>{children}</label>
     <input id={id} type={type} value={value} onChange={onInputChange} />
   </>
 );
